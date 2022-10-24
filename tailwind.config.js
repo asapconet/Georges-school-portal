c1119a1c5d3e734eff3eca41825f410fbc264b02
asapconet <aaronrickymeek@gmail.com>
2022-10-24 16:58:41 +0100

    chore: add styling packages

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{jsx,tsx,js,ts,mdx}",
+    "./pages/**/*.{jsx,tsx,js,ts,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
